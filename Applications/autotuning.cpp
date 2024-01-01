48662a742db6079e72efab10701c64c50103e906
Hooninator <jbellavita@berkeley.edu>
2023-12-31 17:06:33 -0800

    Auotuning scaffolding

---
+++ Applications/autotuning.cpp
@@ -0,0 +1,48 @@
+
+
+#include <cassert>
+#include <string>
+
+#include "CombBLAS/CombBLAS.h"
+#include "CombBLAS/CommGrid3D.h"
+#include "CombBLAS/SpParMat3D.h"
+#include "CombBLAS/ParFriends.h"
+#include "CombBLAS/Autotuning.h"
+
+using namespace combblas;
+
+int main(int argc, char ** argv) {
+    
+    /* ./<binary> <path/to/mat> */
+    
+    assert(argc>1);
+    
+    float dummyBeta = 1.0;
+    float dummyAlpha = 0.1;
+    int cores = 128;
+    int devs = 4;
+    PlatformParams params(dummyAlpha, dummyBeta, cores, devs);
+    
+    std::string matname(argv[1]);
+    
+    int rank; int n;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &n);
+    
+    std::shared_ptr<CommGrid> grid;
+    grid.reset(new CommGrid(MPI_COMM_WORLD,0,0));
+    
+    typedef int64_t IT;
+    typedef double UT;
+    typedef SpDCCols<IT,UT> DER;
+
+    SpParMat<IT,UT,DER> A(grid);
+    A.ParallelReadMM(matname, true, maximum<double>());
+    SpParMat<IT,UT,DER> B(A);
+
+    AutotunerSpGEMM3D<IT,UT,DER,IT,UT,DER> tuner(params, A, B);
+
+    return 0;
+
+}
---
+++ include/CombBLAS/PlatformParams.h
@@ -0,0 +1,45 @@
+
+
+
+#include <exception>
+
+
+namespace combblas {
+
+/* 
+ * TODO: Topology specific params
+ * TODO: intrasocket,intranode alpha/beta
+ */
+
+class PlatformParams {
+public:
+    
+    PlatformParams() {
+        internodeAlpha = MeasureInternodeAlpha();
+        internodeBeta = MeasureInternodeBeta();
+    }
+
+    PlatformParams(float internodeAlpha, float internodeBeta, int coresPerNode, int devsPerNode) : 
+    internodeAlpha(internodeAlpha), internodeBeta(internodeBeta), coresPerNode(coresPerNode), devsPerNode(devsPerNode)
+    {}
+    ~PlatformParams(){}
+    
+    float GetInternodeAlpha() const {return internodeAlpha;} 
+    float GetInternodeBeta() const {return internodeBeta;} 
+    int GetCoresPerNode() const {return coresPerNode;}
+    int GetDevsPerNode() const {return devsPerNode;}
+    
+    float MeasureInternodeAlpha() {throw std::runtime_error("Not implemented");}
+    float MeasureInternodeBeta() {throw std::runtime_error("Not implemented");}
+    
+
+private:
+    
+    float internodeAlpha;
+    float internodeBeta;
+    int coresPerNode;
+    int devsPerNode;
+    
+};
+
+}//combblas
