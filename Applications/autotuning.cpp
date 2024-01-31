93a94acf10b298f778445ab813601c5260c18649
Hooninator <jbellavita@berkeley.edu>
2024-01-31 17:20:06 -0500

    remove random permute

--- Applications/autotuning.cpp
+++ Applications/autotuning.cpp
@@ -42,10 +42,10 @@ int main(int argc, char ** argv) {
     SpParMat<IT,UT,DER> A(grid);
     A.ParallelReadMM(matpath, true, maximum<double>());
     SpParMat<IT,UT,DER> B(A);
-    FullyDistVec<IT,UT> p(A.getcommgrid());
-    p.iota(A.getnrow(), 0);
-    p.RandPerm();
-    (B)(p,p,true);
+    //FullyDistVec<IT,UT> p(A.getcommgrid());
+    //p.iota(A.getnrow(), 0);
+    //p.RandPerm();
+    //(B)(p,p,true);
 
     
     auto resultParams = tuner.TuneSpGEMM3D(A, B, autotuning::BRUTE_FORCE, matpath);
