dbbb3a611389b012df59a5619e296e56d1da20f9
Hooninator <jbellavita@berkeley.edu>
2024-02-21 01:17:18 -0800

    update autotuning driver

--- Applications/autotuning.cpp
+++ Applications/autotuning.cpp
@@ -18,18 +18,20 @@ int main(int argc, char ** argv) {
     
     /* ./<binary> <path/to/mat> */
     
-    assert(argc>1);
+    assert(argc>3);
     
     int rank; int n;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &n);
 
-    std::string matpath(argv[1]);
+    std::string matpathA(argv[1]);
+    std::string matpathB(argv[2]);
+
+    bool permute = (bool)(std::atoi(argv[3]));
     
     autotuning::Init(autotuning::M_SLURM);
     autotuning::Autotuner tuner(autotuning::perlmutterParams);
-    
     
     
     std::shared_ptr<CommGrid> grid;
@@ -40,15 +42,19 @@ int main(int argc, char ** argv) {
     typedef SpDCCols<IT,UT> DER;
 
     SpParMat<IT,UT,DER> A(grid);
-    A.ParallelReadMM(matpath, true, maximum<double>());
-    SpParMat<IT,UT,DER> B(A);
-    //FullyDistVec<IT,UT> p(A.getcommgrid());
-    //p.iota(A.getnrow(), 0);
-    //p.RandPerm();
-    //(B)(p,p,true);
+    SpParMat<IT,UT,DER> B(grid);
+    A.ParallelReadMM(matpathA, true, maximum<double>());
+    B.ParallelReadMM(matpathB, true, maximum<double>());
+    if (permute) {
+        FullyDistVec<IT,UT> p(A.getcommgrid());
+        p.iota(A.getnrow(), 0);
+        p.RandPerm();
+        (B)(p,p,true);
+        matpathB += std::string("-permute");
+    }
 
     
-    auto resultParams = tuner.TuneSpGEMM3D(A, B, autotuning::BRUTE_FORCE, matpath);
+    auto resultParams = tuner.TuneSpGEMM2D(A, B, autotuning::BRUTE_FORCE, matpathA, matpathB);
     
     autotuning::Finalize();
 
