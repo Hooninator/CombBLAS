fbe630ee48facf4a583d4408ec8bbed5e2e5f9d8
Hooninator <jbellavita@berkeley.edu>
2024-03-06 10:20:41 -0800

    add FLOPS to features

---
+++ include/CombBLAS/Autotuning/model/CombBLASSpGEMM.cpp
@@ -0,0 +1,373 @@
+
+
+#include "common.h"
+#include "FeatureExtract.h"
+
+
+#define VERBOSE
+
+#ifdef VERBOSE
+#define PRINT(msg) std::cout<<msg<<std::endl;
+#else
+#define PRINT(msg)
+#endif
+
+#define ITERS 2 //First one doesn't impact the average
+
+#define TIMING
+#define THREADED
+
+#ifdef TIMING
+double cblas_alltoalltime;
+double cblas_allgathertime;
+//////////////////////////
+double mcl_Abcasttime;
+double mcl_Bbcasttime;
+double mcl_localspgemmtime;
+double mcl_multiwaymergetime;
+double mcl_kselecttime;
+double mcl_prunecolumntime;
+double mcl_symbolictime;
+double mcl_totaltime;
+double mcl_tt;
+int64_t mcl_nnzc;
+///////////////////////////
+double mcl_Abcasttime_prev;
+double mcl_Bbcasttime_prev;
+double mcl_localspgemmtime_prev;
+double mcl_multiwaymergetime_prev;
+double mcl_kselecttime_prev;
+double mcl_prunecolumntime_prev;
+double mcl_symbolictime_prev;
+double mcl_totaltime_prev;
+double mcl_tt_prev;
+int64_t mcl_nnzc_prev;
+///////////////////////////
+ double mcl3d_conversiontime;
+ double mcl3d_symbolictime;
+ double mcl3d_Abcasttime;
+ double mcl3d_Bbcasttime;
+ double mcl3d_SUMMAtime;
+ double mcl3d_localspgemmtime;
+ double mcl3d_SUMMAmergetime;
+ double mcl3d_reductiontime;
+ double mcl3d_3dmergetime;
+ double mcl3d_kselecttime;
+
+#endif
+
+
+using namespace combblas;
+
+
+void runSpGEMM1Dcpu(int argc, char ** argv) {
+    //TODO
+}
+
+
+enum SPGEMMALG2D {
+    MEM_EFFICIENT_2D_GPU,
+    MEM_EFFICIENT_2D,
+    DOUBLE_BUFF,
+    SYNCH,
+    OVERLAP,
+    BLOCKED
+} typedef SPGEMMALG2D;
+
+/* 
+ * Run a SpGEMM2D algorithm.
+ * The value of the 2nd command line arg determines which algorithm is run
+ * 0: MemEfficientSpGEMMg
+ * 1: MemEfficientSpGEMM
+ * 2: Mult_AnXBn_DoubleBuff
+ * 3: Mult_AnXBn_Synch
+ * 4: blocked spgemm
+ */
+
+void runSpGEMM2Dcpu(int argc, char ** argv) {
+
+    int np; int rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    /* Setup comm grid  */
+    std::shared_ptr<CommGrid> grid;
+    grid.reset(new CommGrid(MPI_COMM_WORLD,0,0));
+
+    /* Useful types */
+    typedef double NT;
+    typedef int64_t IT;
+    typedef SpDCCols <IT, NT> DER;
+    
+    /* Read in A */
+    SpParMat<IT, NT, DER> A(grid);
+    std::string matNameA(argv[2]);
+    A.ParallelReadMM(matNameA, true, maximum<NT>());
+    
+    double loadBalance = A.LoadImbalance();
+    if (rank==0)
+        PRINT("Load balance A: " + std::to_string(loadBalance) + "\n");
+
+    /* Read in B */
+    SpParMat<IT, NT, DER> B(grid); 
+    std::string matNameB(argv[3]);
+    B.ParallelReadMM(matNameB, true, maximum<NT>());
+
+    bool permute = (bool)(std::atoi(argv[6]));
+    if (permute) {
+        FullyDistVec<IT,NT> p(A.getcommgrid());
+        p.iota(A.getnrow(), 0);
+        p.RandPerm();
+        (B)(p,p,true);
+    }
+
+    loadBalance = B.LoadImbalance();
+    if (rank==0)
+        PRINT("Load balance B: " + std::to_string(loadBalance) + "\n");
+    
+    /* Normal fmadd semiring */
+    typedef PlusTimesSRing<double,double> PTTF;
+    
+    double totalTime=0.0;
+    int64_t perProcessMem = (512) / np; //Perlmutter CPU node
+
+    /* Feature extraction */
+    std::ofstream sampleFile;
+    if (rank==0)
+        sampleFile.open("samples.txt", std::ofstream::app);
+    FeatureExtractor<IT,NT,DER> extractor;
+    std::map<std::string, std::string> * timingsMap = new std::map<std::string,std::string>();
+    
+    int algCode = std::atoi(argv[4]);
+   
+    for (int i=0; i<ITERS; i++) {
+        
+        if (rank==0)
+            PRINT("Iteration " + std::to_string(i)+"\n");
+
+
+        auto stime = MPI_Wtime(); 
+        
+        switch(algCode) {
+        
+            case SPGEMMALG2D::MEM_EFFICIENT_2D_GPU:
+            /*    MemEfficientSpGEMMg<PTTF, NT, DER>(
+                                    A, B, //A, B
+                                    1, //phases
+                                    (NT)(1.0/10000.0), //hardThreshold
+                                    (IT)1100, //selectNum
+                                    (IT)1400, //recoverNum
+                                    (NT)0.9, //recoverPct
+                                    0, //kselectVersion
+                                    perProcessMem, //perProcessMem
+                                    LSPG_NSPARSE, //local_spgemm
+                                    1); //nrounds */
+               break; 
+            
+            case SPGEMMALG2D::MEM_EFFICIENT_2D:
+                MemEfficientSpGEMM<PTTF, NT, DER>(
+                                    A, B, //A, B
+                                    1, //phases
+                                    (NT)(1.0/10000.0), //hardThreshold
+                                    (IT)1100, //selectNum
+                                    (IT)1400, //recoverNum
+                                    (NT)0.9, //recoverPct
+                                    0, //kselectVersion
+                                    2, //computational kernel
+                                    perProcessMem //perProcessMem
+                                    );
+                break;
+            case SPGEMMALG2D::DOUBLE_BUFF:
+                Mult_AnXBn_DoubleBuff<PTTF, NT, DER>(A, B, false,false);
+                break;
+            case SPGEMMALG2D::SYNCH:
+                Mult_AnXBn_Synch<PTTF, NT, DER>(A, B, false, false, timingsMap);
+                break;
+            case SPGEMMALG2D::OVERLAP:
+                Mult_AnXBn_Overlap<PTTF, NT, DER>(A, B);
+                break;
+            case SPGEMMALG2D::BLOCKED:
+            {
+            }
+            default:
+                std::cerr<<"Alg code "<<std::to_string(algCode)<<" not recongized"<<std::endl;
+                exit(1);
+    
+        }
+    
+        
+        auto etime = MPI_Wtime();
+        if (i>0) //First iteration is slow
+            totalTime += (etime-stime);
+        if (rank==0) {
+            timingsMap->emplace("total-time", std::to_string(etime-stime));
+            timingsMap->emplace("A-name", matNameA);
+            if (permute) {
+                timingsMap->emplace("B-name", matNameB+"-permuted");
+            } else {
+                timingsMap->emplace("B-name", matNameB);
+            }
+        }
+    
+        /* Feature extraction only for i>0 */
+        if (i>0)
+            extractor.MakeSample2D(A, B, timingsMap, sampleFile);
+        timingsMap->clear();
+    } 
+
+    delete timingsMap;
+    
+    double avgTime = totalTime / (ITERS-1);
+    
+    if (rank==0) {
+        PRINT("Avg time: " + std::to_string(avgTime) + "s\n");
+        sampleFile.close();
+    }
+
+}
+
+
+enum SPGEMMALG3D {
+    MEM_EFFICIENT_3D,
+    SUMMA_3D
+} typedef SPGEMMALG3D;
+
+/*
+ * Run a 3D SpGEMM algorithm
+ * The value of the second command line argument determines which algorithm is run
+ * 0: MemEfficientSpGEMM3D
+ * 1: Mult_AnXBn_SUMMA3D
+ */
+void runSpGEMM3Dcpu(int argc, char ** argv) {
+    
+    /* MPI init */
+    int np; int rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    /* Comm Grid setup */
+    std::shared_ptr<CommGrid> grid;
+    grid.reset(new CommGrid(MPI_COMM_WORLD,0,0));
+    
+    
+    /* Important types */
+    typedef  int64_t IT;
+    typedef  double NT;
+    typedef SpDCCols <IT, NT> DER;
+    typedef PlusTimesSRing<NT, NT> PTTF;
+    
+    /* Read in A */
+    SpParMat<IT, NT, DER> Atemp(grid); 
+    std::string matNameA(argv[2]);
+    Atemp.ParallelReadMM(matNameA, true, maximum<NT>());
+    
+    /* Read in B */
+    SpParMat<IT, NT, DER> Btemp(grid); 
+    std::string matNameB(argv[3]);
+    Btemp.ParallelReadMM(matNameB, true, maximum<NT>());
+    
+    /* Convert A and B to 3D */
+    int nLayers = std::atoi(argv[5]);
+    SpParMat3D<IT,NT,DER> A(Atemp,nLayers, true);
+    SpParMat3D<IT,NT,DER> B(Btemp,nLayers, false);
+
+    double loadBalance = Atemp.LoadImbalance();
+    if (rank==0)
+        PRINT("Load balance A: " + std::to_string(loadBalance));
+
+    loadBalance = Btemp.LoadImbalance();
+    if (rank==0)
+        PRINT("Load balance B: " + std::to_string(loadBalance));
+    
+    PRINT("Local nnz of A on rank " + std::to_string(rank) + ": " + std::to_string(A.seqptr()->getnnz()) );
+    PRINT("Local nnz of B on rank " + std::to_string(rank) + ": " + std::to_string(B.seqptr()->getnnz()) );
+    
+    IT localFLOPS = 0;
+
+    EstimateFLOP<PTTF, IT, NT, NT, DER, DER>(*(A.GetLayerMat()), *(B.GetLayerMat()), false, false, &localFLOPS);
+    
+    PRINT("Local FLOPS on rank " + std::to_string(rank) + ": " + std::to_string(localFLOPS) );
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    double totalTime = 0.0;
+    double perProcessMem = (512)/np;
+    int algCode = std::atoi(argv[4]);
+    
+    for (int i=0;i<ITERS;i++) {
+        
+        if (rank==0)
+            PRINT("Iteration " + std::to_string(i) + "\n");
+        
+        auto stime = MPI_Wtime();
+        
+        switch (algCode) {
+        
+            case SPGEMMALG3D::MEM_EFFICIENT_3D:
+                MemEfficientSpGEMM3D<PTTF, NT, DER>(
+                                    A, //A 
+                                    B, //B
+                                    1, //phases
+                                    (NT)(1.0/10000.0), //hardThreshold
+                                    (IT)1100, //selectNum
+                                    (IT)1400, //recoverNum
+                                    (NT)0.9, //recoverPct
+                                    0, //kselectVersion
+                                    2, //computational kernel
+                                    perProcessMem //perProcessMem
+                                    );
+                break;
+            case SPGEMMALG3D::SUMMA_3D:
+                //This does not have an implementation on the combblas-gpu branch
+                Mult_AnXBn_SUMMA3D<PTTF, NT, DER>(A,B);
+                break;
+            default:
+                std::cerr<<"Algorithm code "<<algCode<<" not valid"<<std::endl;
+                exit(1);
+                break;
+
+        }
+    
+        auto etime = MPI_Wtime();
+        
+        if (i>0) //First iteration is slow
+            totalTime += (etime - stime);
+
+    }
+    
+    double avgTime = totalTime / (ITERS-1);
+    
+    if (rank==0)
+        PRINT("Avg time: " + std::to_string(avgTime) + "s\n");
+    
+    
+
+}
+
+
+int main(int argc, char ** argv) {
+    
+    /* ./binary <ALG-TYPE> <MAT-NAME-A> <MAT-NAME-B> <ALG-CODE> <LAYERS> <PERMUTE-B>*/
+
+    assert(argc>6);
+    
+    std::string algType(argv[1]);
+    
+    if (algType.compare("2D")==0) {
+        runSpGEMM2Dcpu(argc, argv);
+    } else if (algType.compare("3D")==0) {
+        runSpGEMM3Dcpu(argc, argv);
+    } else if (algType.compare("1D")==0) {
+        runSpGEMM1Dcpu(argc, argv);
+    } else {
+        std::cerr<<"Algorithm type "<<algType<<" not recognized"<<std::endl;
+        exit(1);
+    }
+    
+    
+    return 0;
+
+}
+
---
+++ include/CombBLAS/Autotuning/model/FeatureExtract.h
@@ -0,0 +1,416 @@
+
+#ifndef FEATURE_EXTRACT_H
+#define FEATURE_EXTRACT_H
+
+
+#include "common.h"
+
+#define PRECISION 20
+
+using namespace combblas;
+
+
+template <typename IT, typename NT, typename DER>
+class FeatureExtractor {
+
+typedef std::map<std::string, std::string> Map;
+
+public:
+
+FeatureExtractor(){}
+
+
+void MakeSample2D(SpParMat<IT,NT,DER>& A, SpParMat<IT,NT,DER>& B, Map * timings, std::ofstream& ofs) {
+
+    int rank = A.getcommgrid()->GetRank();
+    if (rank==0)
+        std::cout<<"Making sample..."<<std::endl;
+    
+    Map * featMap = new Map();
+
+    std::string Astr("A");
+    std::string Bstr("B");
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Job stats
+    JobStats(featMap);
+
+    // Global matrix stats
+    GlobStats(A, featMap, Astr);
+    GlobStats(B, featMap, Bstr);
+    if (rank==0)
+        std::cout<<"Extracted global stats..."<<std::endl;
+
+    // Per column stats 
+    ColNnzStats(A, featMap, Astr);
+    ColNnzStats(B, featMap, Bstr);
+    if (rank==0)
+        std::cout<<"Extracted column stats..."<<std::endl;
+    
+    // Tile stats
+    TileNnzStats(A, featMap, Astr);
+    TileNnzStats(B, featMap, Bstr);
+    if (rank==0)
+        std::cout<<"Extracted tile stats..."<<std::endl;
+
+    // Tile col stats
+    TileColNnzStats(A, featMap, Astr);
+    TileColNnzStats(B, featMap, Bstr);
+    if (rank==0)
+        std::cout<<"Extracted tile col stats..."<<std::endl;
+
+    // FLOP stats
+    typedef PlusTimesSRing<NT,NT> PTTF;
+    IT localFLOPS, globalFLOPS = 0;
+    globalFLOPS = EstimateFLOP<PTTF, IT, NT, NT, DER, DER>(A, B, false, false, &localFLOPS);
+
+    if (rank==0) {
+        featMap->emplace("local-FLOPS", STR(localFLOPS));
+        featMap->emplace("global-FLOPS", STR(globalFLOPS));
+        std::cout<<"Extracted FLOP stats..."<<std::endl;
+    }
+
+
+    if (rank==0) {
+        WriteSample(featMap, timings, ofs);
+        std::cout<<"Wrote sample..."<<std::endl;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+}
+
+
+void WriteSample(const Map * features, const Map * timings, std::ofstream& ofs) {
+
+    // Write features
+    std::for_each(features->begin(), features->end(),
+        [&ofs](auto const& elem) {
+            ofs<<elem.first<<":"<<elem.second<<" ";
+        }
+    );
+
+    // Write timings
+    std::for_each(timings->begin(), timings->end(),
+        [&ofs](auto const& elem) {
+            ofs<<elem.first<<":"<<elem.second<<" ";
+        }
+    );
+
+    // Newline, end of feature
+    ofs<<std::endl;
+
+}
+
+
+template <typename T>
+std::string ToStrScientific(T input) {
+    std::ostringstream ss;
+    ss<<std::scientific<<std::setprecision(PRECISION)<<input;
+    return ss.str();
+}
+
+
+void JobStats(Map * featMap) {
+    featMap->emplace("Nodes", std::getenv("SLURM_NNODES"));
+    featMap->emplace("PPN", std::getenv("SLURM_NTASKS_PER_NODE"));
+}
+
+
+void GlobStats(SpParMat<IT,NT,DER>& Mat, Map * featMap, std::string& matSymb) {
+    int rank = Mat.getcommgrid()->GetRank();
+    IT nnz, nrow, ncol;
+    nnz = Mat.getnnz();
+    nrow = Mat.getnrow();
+    ncol = Mat.getncol();
+    if (rank==0) { 
+        featMap->emplace("nnz-"+matSymb, ToStrScientific(nnz));
+        featMap->emplace("m-"+matSymb, ToStrScientific(nrow));
+        featMap->emplace("n-"+matSymb, ToStrScientific(ncol));
+        featMap->emplace("density-"+matSymb, 
+                            ToStrScientific(static_cast<float>(nnz) / static_cast<float>(ncol * nrow) ) );
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+
+// avg nnz, min, max, stdev per col
+void ColNnzStats(SpParMat<IT,NT,DER>& Mat, Map * featMap, std::string& matSymb) {
+
+    int rank = Mat.getcommgrid()->GetRank();
+
+    // Average nnz per column
+    double avgNnz = static_cast<double>(Mat.getnnz()) / static_cast<double>(Mat.getncol());
+    
+    // Min and max nnz per col -- requires communication since matrix is distributed
+    // Populate array with local nnz counts
+    std::vector<IT> locNnzColArr(Mat.seqptr()->getncol());
+    for (auto colIter = Mat.seqptr()->begcol(); colIter!=Mat.seqptr()->endcol(); colIter++) {
+        locNnzColArr[colIter.colid()] = colIter.nnz();
+    }
+
+    // Allreduce along each processor column
+    MPI_Allreduce(MPI_IN_PLACE, (void*)(locNnzColArr.data()), locNnzColArr.size(),
+                    MPIType<IT>(), MPI_SUM, Mat.getcommgrid()->GetColWorld());
+
+    //Setup gatherv
+    std::vector<int> recvCounts(Mat.getcommgrid()->GetGridCols());
+    int locNcols = Mat.seqptr()->getncol();
+    MPI_Allgather((void*)(&locNcols), 1, MPI_INT, (void*)(recvCounts.data()), 1, MPI_INT, 
+                    Mat.getcommgrid()->GetRowWorld());
+
+    std::vector<int> displs(Mat.getcommgrid()->GetGridCols());
+    for (int i=1; i<displs.size(); i++) {
+        displs[i] = displs[i-1] + recvCounts[i-1];
+    }
+
+    // Gatherv along each processor row to get global col nnz
+    // NOTE: Gatherv is needec because the last processor column can have edge case columns
+    std::vector<IT> globNnzColArr(Mat.getncol());
+    MPI_Gatherv((void*)(locNnzColArr.data()), locNnzColArr.size(), MPIType<IT>(),
+                    (void*)(globNnzColArr.data()), recvCounts.data(), displs.data(), MPIType<IT>(),
+                    0, Mat.getcommgrid()->GetRowWorld());
+
+    IT nrows = Mat.getnrow();
+    IT ncols = Mat.getnrow();
+
+    if (rank==0) {
+        // Reduce to get min and max
+        IT minNnzCol, maxNnzCol;
+        minNnzCol = ReduceMin(globNnzColArr);
+        maxNnzCol = ReduceMax(globNnzColArr);
+
+        // Reduce to get stdev
+        double stdev;
+        stdev = ReduceStdev(globNnzColArr, avgNnz);
+
+        // Compute density info
+        double density, avgDensity, minDensity, maxDensity, stdevDensity;
+        std::vector<double> colDensities(ncols);
+        std::transform(globNnzColArr.begin(), globNnzColArr.end(), colDensities.begin(),
+                        [&nrows](IT colNnz) {return static_cast<double>(colNnz)/static_cast<double>(nrows);});
+
+        avgDensity = ReduceMean(colDensities);
+        minDensity = ReduceMin(colDensities);
+        maxDensity = ReduceMax(colDensities);
+        stdevDensity = ReduceStdev(colDensities, avgDensity);
+
+        // Write values to featMap
+        featMap->emplace("minNnzCol-"+matSymb, ToStrScientific(minNnzCol));
+        featMap->emplace("maxNnzCol-"+matSymb, ToStrScientific(maxNnzCol));
+        featMap->emplace("stdevNnzCol-"+matSymb, ToStrScientific(stdev));
+        featMap->emplace("avgNnzCol-"+matSymb, ToStrScientific(avgNnz));
+        featMap->emplace("avgDensityCol-"+matSymb, ToStrScientific(avgDensity));
+        featMap->emplace("minDensityCol-"+matSymb, ToStrScientific(minDensity));
+        featMap->emplace("maxDensityCol-"+matSymb, ToStrScientific(maxDensity));
+        featMap->emplace("stdevDensityCol-"+matSymb, ToStrScientific(stdevDensity));
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+}
+
+//Average, min, max, stdev, density for nnz per tile
+void TileNnzStats(SpParMat<IT,NT,DER>& Mat, Map * featMap, std::string& matSymb) {
+
+    int rank = Mat.getcommgrid()->GetRank();
+
+    double avgNnzTile = (static_cast<double>(Mat.getnnz()) / static_cast<double>(Mat.getcommgrid()->GetSize()) );
+
+    IT locNrows = Mat.seqptr()->getnrow();
+    IT locNcols = Mat.seqptr()->getncol();
+
+    double density = static_cast<double>(Mat.seqptr()->getnnz()) / 
+                        static_cast<long double>(Mat.seqptr()->getncol() * Mat.seqptr()->getnrow());
+    std::vector<double> tileDensities(Mat.getcommgrid()->GetSize());
+    MPI_Allgather((void*)(&density), 1, MPI_DOUBLE, (void*)(tileDensities.data()), 1, MPI_DOUBLE, Mat.getcommgrid()->GetWorld());
+
+    double avgDensity = ReduceMean(tileDensities);
+    double minDensity = ReduceMin(tileDensities);
+    double maxDensity = ReduceMax(tileDensities);
+    double stdevDensity = ReduceStdev(tileDensities, avgDensity);
+
+    std::vector<IT> globNnzTile(Mat.getcommgrid()->GetSize());
+    IT locNnzTile = Mat.seqptr()->getnnz();
+
+    // Gather to get complete list of tile nnz counts
+    MPI_Gather((void*)(&locNnzTile), 1, MPIType<IT>(), 
+                     (void*)(globNnzTile.data()), 1, MPIType<IT>(), 
+                        0, Mat.getcommgrid()->GetWorld());
+    
+    if (rank==0) {
+        // Get min and max nnz
+        IT minNnzTile, maxNnzTile;
+        minNnzTile = ReduceMin(globNnzTile);
+        maxNnzTile = ReduceMax(globNnzTile);
+
+        // Reduce to get stdev
+        double stdev;
+        stdev = ReduceStdev(globNnzTile, avgNnzTile);
+
+        // Write values to featMap
+        featMap->emplace("mTile-"+matSymb, ToStrScientific(locNrows));
+        featMap->emplace("nTile-"+matSymb, ToStrScientific(locNcols));
+        featMap->emplace("minNnzTile-"+matSymb, ToStrScientific(minNnzTile));
+        featMap->emplace("maxNnzTile-"+matSymb, ToStrScientific(maxNnzTile));
+        featMap->emplace("stdevNnzTile-"+matSymb, ToStrScientific(stdev));
+        featMap->emplace("avgNnzTile-"+matSymb, ToStrScientific(avgNnzTile));
+        featMap->emplace("avgDensityTile-"+matSymb, ToStrScientific(avgDensity));
+        featMap->emplace("minDensityTile-"+matSymb, ToStrScientific(minDensity));
+        featMap->emplace("maxDensityTile-"+matSymb, ToStrScientific(maxDensity));
+        featMap->emplace("stdevDensityTile-"+matSymb, ToStrScientific(stdevDensity));
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+}
+
+
+void TileColNnzStats(SpParMat<IT,NT,DER>& Mat, Map * featMap, std::string& matSymb) {
+
+    int rank = Mat.getcommgrid()->GetRank();
+
+    IT n = Mat.getncol() * Mat.getcommgrid()->GetGridRows();
+
+    double avgNnzTileCol = static_cast<double>(Mat.getnnz()) / static_cast<double>(n);
+    
+    // Local tile col nnz
+    std::vector<IT> * locTileColNnz = new std::vector<IT>(Mat.seqptr()->getncol());
+    for (auto colIter = Mat.seqptr()->begcol(); colIter!=Mat.seqptr()->endcol(); colIter++) {
+        locTileColNnz->at(colIter.colid()) = colIter.nnz();
+    }
+
+
+    // Setup gatherv
+    std::vector<int> * recvCounts = new std::vector<int>(Mat.getcommgrid()->GetGridCols());
+    int locNcols = Mat.seqptr()->getncol();
+    MPI_Allgather((void*)(&locNcols), 1, MPI_INT, (void*)(recvCounts->data()), 1, MPI_INT, Mat.getcommgrid()->GetRowWorld()); 
+
+    std::vector<int> * displs = new std::vector<int>(Mat.getcommgrid()->GetGridCols());
+    for (int i=1; i<displs->size(); i++) {
+        displs->at(i) = displs->at(i-1) + recvCounts->at(i-1);
+    }
+
+    //if (rank==0) std::cout<<"Starting gather"<<std::endl;
+    // Aggregate all tile col nnz on processor column 0
+    std::vector<IT> * rowWorldTileColNnz = new std::vector<IT>(Mat.getncol());
+    MPI_Gatherv((void*)(locTileColNnz->data()), locTileColNnz->size(), MPIType<IT>(),
+                (void*)(rowWorldTileColNnz->data()), recvCounts->data(), displs->data(), MPIType<IT>(),
+                0, Mat.getcommgrid()->GetRowWorld());
+    //if (rank==0) std::cout<<"Done with gather"<<std::endl;
+
+    // Avoid creating large n*ncols arrays
+    IT locNrows = Mat.seqptr()->getnrow();
+    IT ncols = Mat.getncol();
+    if (Mat.getcommgrid()->GetRankInProcRow()==0) {
+
+        // Local operations 
+        IT locMinNnzTileCol = ReduceMin(*rowWorldTileColNnz);
+        IT locMaxNnzTileCol = ReduceMax(*rowWorldTileColNnz);
+        long double locStdevContrib = std::pow(ReduceStdev(*rowWorldTileColNnz, avgNnzTileCol) * rowWorldTileColNnz->size(), 2);
+
+        // Reduce among processor column 0 to get complete values on rank 0
+        IT minNnzTileCol, maxNnzTileCol;
+        long double stdev;
+        MPI_Reduce((void*)(&locMinNnzTileCol), (void*)(&minNnzTileCol), 1, MPIType<IT>(),
+                    MPI_MIN, 0, Mat.getcommgrid()->GetColWorld());
+        MPI_Reduce((void*)(&locMaxNnzTileCol), (void*)(&maxNnzTileCol), 1, MPIType<IT>(),
+                    MPI_MAX, 0, Mat.getcommgrid()->GetColWorld());
+        MPI_Reduce((void*)(&locStdevContrib), (void*)(&stdev), 1, MPI_LONG_DOUBLE,
+                    MPI_SUM, 0, Mat.getcommgrid()->GetColWorld());
+        stdev = std::sqrt(stdev) / static_cast<double>(n);
+
+        // Compute density info
+        double locDensitySum, locMinDensity, locMaxDensity, locStdevDensityContrib;
+        double avgDensity, minDensity, maxDensity, stdevDensity;
+        std::vector<double> * tileColDensities = new std::vector<double>(ncols);
+        std::transform(rowWorldTileColNnz->begin(), rowWorldTileColNnz->end(), tileColDensities->begin(),
+            [&locNrows](IT tileColNnz){return static_cast<double>(tileColNnz) / static_cast<double>(locNrows);}); 
+        
+        locMinDensity = ReduceMin(*tileColDensities);
+        locMaxDensity = ReduceMax(*tileColDensities);
+        MPI_Reduce((void*)(&locMinDensity), (void*)(&minDensity), 1, MPI_DOUBLE, MPI_MIN, 0, Mat.getcommgrid()->GetColWorld());
+        MPI_Reduce((void*)(&locMaxDensity), (void*)(&maxDensity), 1, MPI_DOUBLE, MPI_MAX, 0, Mat.getcommgrid()->GetColWorld());
+
+        // sum all densities
+        locDensitySum = std::reduce(tileColDensities->begin(), tileColDensities->end(), 0);
+        MPI_Allreduce(MPI_IN_PLACE, (void*)(&locDensitySum), 1, MPI_DOUBLE, MPI_SUM, Mat.getcommgrid()->GetColWorld());
+
+        // Compute avg
+        avgDensity = locDensitySum / static_cast<double>(n);
+
+
+        // Finally, compute stdev
+        locStdevDensityContrib = std::pow(ReduceStdev(*tileColDensities, avgDensity) * tileColDensities->size(), 2);
+        MPI_Reduce((void*)(&locStdevDensityContrib), (void*)(&stdevDensity), 1, MPI_DOUBLE, MPI_SUM, 0, Mat.getcommgrid()->GetColWorld());
+        stdevDensity = std::sqrt(stdev) / static_cast<double>(n);
+
+        if (rank==0) {
+            // Write values to featMap
+            featMap->emplace("minNnzTileCol-"+matSymb, ToStrScientific(minNnzTileCol));
+            featMap->emplace("maxNnzTileCol-"+matSymb, ToStrScientific(maxNnzTileCol));
+            featMap->emplace("stdevNnzTileCol-"+matSymb, ToStrScientific(stdev));
+            featMap->emplace("avgNnzTileCol-"+matSymb, ToStrScientific(avgNnzTileCol));
+            featMap->emplace("avgDensityTileCol-"+matSymb, ToStrScientific(avgDensity));
+            featMap->emplace("minDensityTileCol-"+matSymb, ToStrScientific(minDensity));
+            featMap->emplace("maxDensityTileCol-"+matSymb, ToStrScientific(maxDensity));
+            featMap->emplace("stdevDensityTileCol-"+matSymb, ToStrScientific(stdevDensity));
+        }
+
+        delete tileColDensities;
+    }
+
+    delete locTileColNnz;
+    delete rowWorldTileColNnz;
+    delete recvCounts;
+    delete displs;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+}
+
+
+template <typename T>
+T ReduceMin(std::vector<T>& v) {
+    return std::reduce(v.begin(), v.end(), T(0),
+        [](T currMin, T curr) {return std::min(currMin, curr);}
+    );
+}
+
+
+template <typename T>
+T ReduceMax(std::vector<T>& v) {
+    return std::reduce(v.begin(), v.end(), T(0),
+        [](T currMax, T curr) {return std::max(currMax, curr);}
+    );
+}
+
+
+template <typename T>
+double ReduceStdev(std::vector<T>& v, double avg) {
+    return std::sqrt(std::reduce(v.begin(), v.end(), 0.0,
+        [&avg](double sum, T val) {
+            return sum + (std::pow( static_cast<double>((val - avg)), 2)); 
+        }
+    ) / v.size());
+}
+
+
+template <typename T>
+T ReduceMean(std::vector<T>& v) {
+    return std::reduce(v.begin(), v.end(), T(0))/static_cast<T>(v.size());
+}
+
+
+};
+
+#endif
+
+
+
+
+
+
+
+
