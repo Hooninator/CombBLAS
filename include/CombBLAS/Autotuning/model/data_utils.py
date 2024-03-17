6b847fc887f57bc317e914fad7251df6cf5ea66f
Hooninator <jbellavita@berkeley.edu>
2024-03-16 19:29:56 -0700

    phase model

--- include/CombBLAS/Autotuning/model/data_utils.py
+++ include/CombBLAS/Autotuning/model/data_utils.py
@@ -2,6 +2,11 @@ from collections import defaultdict
 
 import pandas as pd
 
+import os
+import time
+
+path_prefix="/global/homes/j/jbellav/CombBLAS/include/CombBLAS/Autotuning/model/"
+
 
 def load_spgemm2d_data(filename="samples.txt"):
     
@@ -47,9 +52,41 @@ def load_spgemm2d_data(filename="samples.txt"):
     return df
 
 
+def load_gnn_df(features, labels, f_prefix="samples-gnn-mod"):
+    
+    graph_list = []
+    
+    file_names = list(filter(lambda s: f_prefix in s, os.listdir(path_prefix)))
+    
+    df_dict = defaultdict(lambda: [])
 
-def get_problem_feature(df, matnameA, matnameB, feature):
-    
-    
-    
-    return
+    stime = time.time()
+    for fname in file_names:
+        print(f"Processing {fname}...")
+        with open(path_prefix+fname, 'r') as file:
+            for line in file:
+                if "SAMPLE" in line:
+                    continue
+                feats = line.split(" ")
+                found_feats = []
+                for f in feats:
+                    name, val = f.split(":")[0], f.split(":")[1]
+                    name = name.strip()
+                    if "problem" in name:
+                        df_dict[name].append(val)
+                        found_feats.append(name)
+                    elif name in features+labels and name not in found_feats:
+                        try:
+                            df_dict[name].append(float(val))
+                            found_feats.append(name)
+                        except:
+                            continue
+                for f in features+labels+["problem"]:
+                    if f not in found_feats:
+                        df_dict[f].append(None)
+    etime = time.time()
+    df = pd.DataFrame(df_dict)
+    print(f"Processsed {len(df)} samples in {etime-stime}s")
+
+    df["bcast"] = df["bcast-A"] + df["bcast-B"]
+    return df 
--- include/CombBLAS/Autotuning/model/spgemm_phase_model_xgb.py
+++ include/CombBLAS/Autotuning/model/spgemm_phase_model_xgb.py
@@ -33,7 +33,8 @@ features = ["FLOPS",
             "outputNnz-intermediate",
             "outputNnz-final",
             "Nodes",
-            "PPN"
+            "PPN",
+            "rank"
             ]
 labels = [
         "bcast-A",
@@ -52,45 +53,6 @@ class PlatformParams:
     inter_alpha: float
 
 perlmutter_params = PlatformParams(2398.54, 3.9)
-
-
-def load_gnn_df(f_prefix="samples-gnn-mod"):
-    
-    graph_list = []
-    
-    file_names = list(filter(lambda s: f_prefix in s, os.listdir(path_prefix)))
-    
-    df_dict = defaultdict(lambda: [])
-
-    stime = time.time()
-    for fname in file_names:
-        print(f"Processing {fname}...")
-        with open(path_prefix+fname, 'r') as file:
-            for line in file:
-                if "SAMPLE" in line:
-                    continue
-                feats = line.split(" ")
-                found_feats = []
-                for f in feats:
-                    name, val = f.split(":")[0], f.split(":")[1]
-                    name = name.strip()
-                    if "problem" in name:
-                        df_dict[name].append(val)
-                        found_feats.append(name)
-                    elif name in features+labels and name not in found_feats:
-                        try:
-                            df_dict[name].append(float(val))
-                            found_feats.append(name)
-                        except:
-                            continue
-                for f in features+labels+["problem"]:
-                    if f not in found_feats:
-                        df_dict[f].append(None)
-    etime = time.time()
-    df = pd.DataFrame(df_dict)
-    print(f"Processsed {len(df)} samples in {etime-stime}s")
-
-    return df 
 
 
 def train_model_xgb(args, train_data, test_data):
@@ -128,126 +90,257 @@ def train_model_xgb(args, train_data, test_data):
     plt.savefig(f"{args.plotname}-loss", bbox_inches='tight')
 
 
-def eval_xgb(args, model, df, features):
-    
-    
-    os.system("rm -f plots-xgb")
-    
-    kendall_sum = 0
-    n_kendall = 0
-    rmse_sum = 0
-    diff_sum = 0
-    n_correct = 0
-    top_1_err = 0
-    n_top_1 = 0
+class ProblemPhaseResults:
 
-    problems = df['problem'].unique()
 
+    def __init__(self):
+        self.results = {}
+
+    @dataclass
+    class Result:
+        problem:str
+        rmse:float
+        kt:float
+        diff:float
+        top1err:float
+        correct:int
+
+    def add_result(self, problem, y_arr, y_pred_arr):
+
+        y_arr, y_pred_arr = np.array(y_arr), np.array(y_pred_arr)
+
+        kt = (kendalltau(y_pred_arr, y_arr).statistic)
+
+        rmse = ((((np.linalg.norm(y_pred_arr - y_arr))**(1/2))/len(y_arr))**2)
+
+        is_correct = 1 if np.argmin(y_pred_arr)==np.argmin(y_arr) else 0
+
+        diff = abs(y_arr[np.argmin(y_pred_arr)] - np.min(y_arr))
+
+        if np.min(y_arr)>0:
+            top_1_err = (y_arr[np.argmin(y_pred_arr)] / (np.min(y_arr))) - 1
+        else:
+            top_1_err = None
+
+        self.results[problem] = self.Result(problem, rmse, kt, diff, top_1_err, is_correct)
+    
+    def get_stat_arr(self, stat_name):
+        arr = map(lambda r: self.results[r].__dict__[stat_name], self.results)
+        return list(filter(lambda x: x!=None, arr)) 
+
+    def get_result_stat(self, problem, stat):
+        return self.results[problem].__dict__[stat]
+
+    def output_eval(self):
+
+        kt_arr = self.get_stat_arr("kt")
+        rmse_arr = self.get_stat_arr("rmse")
+        diff_arr = self.get_stat_arr("diff")
+        correct_arr = self.get_stat_arr("correct")
+        err_arr = self.get_stat_arr("top1err")
+        
+        print(f"----AVERAGE KT: {sum(kt_arr)/len(kt_arr)}")
+        
+        kt_sorted_results = sorted(self.results.values(), key=lambda r:r.kt)
+        print(f"----Problems with the 10 worst KT are: ")
+        for i in range(0, 10):
+            print(f"{kt_sorted_results[i].problem}")
+
+        print(f"----Problems with the 10 best KT are: ")
+        for i in range(1, 11):
+            print(f"{kt_sorted_results[-i].problem}")
+
+        print(f"----AVERAGE DIFF : {sum(diff_arr)/len(diff_arr)}s")
+        print(f"----TOTAL DIFF : {sum(diff_arr)}s")
+        print(f"----NUMBER CORRECT : {sum(correct_arr)}/{len(correct_arr)}")
+        print(f"----TOTAL TOP 1 ERROR: {sum(err_arr)}")
+        print(f"----AVERAGE TOP 1 ERROR: {sum(err_arr)/len(err_arr)}")
+    
+
+def eval_phase(args, test_df, model, bulk=True):
+    
+    os.system(f"rm -f {args.label}-plots/*")
+
+    test_df['params'] = test_df.apply(lambda row: f"{row['Nodes']}, {row['PPN']}", axis=1)
+    test_df['processes'] = test_df.apply(lambda row: f"{row['Nodes']*row['PPN']}", axis=1)
+
+    problems = test_df['problem'].unique()
+
+    results = ProblemPhaseResults()
+    i = 0
+    
     print(f"Evaluating {len(problems)} problems")
-
     for problem in problems:
 
-        print(f"Evaluating {problem}")
-        
-        target_dir = "plots-xgb/"
-        
-        df_problem = df[df['problem']==problem].copy()
-        X = df_problem[features]
-        y = df_problem[args.label].values
+        if i%10==0:
+            print(f"{i}/{len(problems)} evaluated...")
 
-        print(df_problem["Nodes"].unique())
+        df_problem = test_df[test_df['problem']==problem]
         
-        y_pred = model(X)
+        params = df_problem['params'].unique()
+        
+        y_pred_arr = []
+        y_arr = []
+        processes = []
+        valid_params = []
 
-        df_problem['pred'] = y_pred
+        if bulk:
 
-        df_pred = df_problem.groupby(['Nodes', 'PPN']).max()
-        y_pred, y = np.array(df_pred['pred']), np.array(df_pred[args.label])
-        
-        is_correct = 1 if np.argmin(y_pred)==np.argmin(y) else 0
-        diff = abs(y[np.argmin(y_pred)] - np.min(y))
-        kendall = kendalltau(y, y_pred)
-        rmse = ((np.linalg.norm(y - y_pred)**2)/len(y))**(1/2)
-        
-        
-        if kendall!=float('nan'):
-            kendall_sum += kendall.correlation
-            n_kendall += 1
+            stime = time.time()
 
-        rmse_sum += rmse
-        diff_sum += diff
-        n_correct += is_correct
-        if np.min(y)>0:
-            top_1_err += (y[np.argmin(y_pred)] / (np.min(y))) - 1
-            n_top_1 += 1
+            X = df_problem[features]
+            y = df_problem[args.label]
+            y_pred = model(X)
+
+            df_problem['pred'] = y_pred
             
+            df_max = df_problem.groupby(by=["Nodes","PPN"]).max()
+            y_pred_arr = df_max['pred']
+            y_arr = df_max[args.label]
 
-        plt.scatter(range(len(y)), y, label="Actual")
-        plt.scatter(range(len(y)), y_pred, label=f"Predicted\n(kt={kendall.correlation})\n(rmse={rmse})")
-        plt.ylabel("Runtime (s)")
-        plt.yscale("log")
-        plt.xlabel("Sample index")
-        plt.title(f"Actual vs. Predicted Runtime for {problem}")
-        plt.legend()
-        plt.savefig(f"{target_dir}{args.plotname}-{problem}.png")
-        plt.clf()
-    
-    print(f"----AVERAGE KT : {kendall_sum/n_kendall}")
-    print(f"----AVERAGE RMSE : {rmse_sum/len(problems)}")
-    print(f"----AVERAGE DIFF : {diff_sum/len(problems)}s")
-    print(f"----TOTAL DIFF : {diff_sum}s")
-    print(f"----NUMBER CORRECT : {n_correct}/{len(problems)}")
-    print(f"----TOTAL TOP 1 ERROR: {top_1_err}")
-    print(f"----AVERAGE TOP 1 ERROR: {top_1_err/n_top_1}")
-    
+            valid_params = params
+
+            etime = time.time()
+            print(f"Time for inference: {etime-stime}s")
+        else:
+            for param in params:
+
+                df_problem_params = df_problem[df_problem['params']==param]
+
+                if not math.sqrt(df_problem_params.shape[0]).is_integer():
+                    print("erroneous grid")
+                    continue
+
+                stime = time.time()
+                X = df_problem_params[features]
+                y = df_problem_params[args.label]
+                y_pred = model(X)
+                y_pred_arr.append(max(y_pred))
+                y_arr.append(max(y))
+                processes.append(df_problem_params['processes'].values[0])
+                valid_params.append(param)
+                etime = time.time()
+                print(f"Time for inference: {etime-stime}s")
+        
+
+        results.add_result(problem, y_arr, y_pred_arr)
+
+        if args.plotname:
+            kt = results.get_result_stat(problem, "kt")
+            rmse = results.get_result_stat(problem, "rmse")
+            plt.scatter(valid_params, y_arr, label="Actual", marker='.',s=100)
+            plt.scatter(valid_params, y_pred_arr, label=f"Predicted\n(kt={kt})\n(rmse={rmse})", marker='.',s=100)
+            plt.ylabel("Runtime (s)")
+            plt.yscale("log")
+            plt.xlabel("Params")
+            plt.xticks(rotation='vertical')
+            plt.title(f"Actual vs. Predicted Runtime for {problem} {args.label}")
+            plt.legend()
+            plt.savefig(f"{args.label}-plots/{args.plotname}-{problem}.png", bbox_inches='tight')
+            plt.clf()
+
+        i+=1
+
+    results.output_eval()
+
 
 def split(df, size):    
-    s = int(len(df)*size)
-    test, train = df.iloc[0:s,:], df.iloc[s:,:]
+    problems = df['problem'].unique()
+    s = int(len(problems)*size)
+
+    random.shuffle(problems)
+    print(len(problems))
+
+    test_problems, train_problems = problems[:s],problems[s:]
+    
+    test, train = df[df['problem'].isin(test_problems)], df[df['problem'].isin(train_problems)]
     return test,train 
 
 
-def loc_mult_model(X):
-    bst = xgb.Booster()
-    bst.load_model(f"{path_prefix}models/xgb-mult.model")
+def loc_mult_model(X, use_xgb=True):
+    X = X.drop(labels=["rank"], axis=1)
+    if use_xgb:
+        bst = xgb.Booster()
+        bst.load_model(f"{path_prefix}models/xgb-mult.model")
+        return np.array(bst.predict(xgb.DMatrix(X)))
+    else:
+        return 0
 
-    return np.array(bst.predict(xgb.DMatrix(X)))
 
-
-def merge_model(X):
-    bst = xgb.Booster()
-    bst.load_model(f"{path_prefix}models/xgb-merge.model")
-
-    return np.array(bst.predict(xgb.DMatrix(X)))
-
+def merge_model(X, use_xgb=True):
+    X = X.drop(labels=["rank"], axis=1)
+    if use_xgb:
+        bst = xgb.Booster()
+        bst.load_model(f"{path_prefix}models/xgb-merge.model")
+        return np.array(bst.predict(xgb.DMatrix(X)))
+    else:
+        return 0
 
 def bcast_model(X):
 
-    result_arr = []
+    def bcast_tree(grid_dim, msg_size):
+        alpha = math.log2(grid_dim)*perlmutter_params.inter_alpha
+        beta = math.log2(grid_dim)*(total_bytes / perlmutter_params.inter_beta)
+        return (alpha+beta) / 1e6
 
+    def bcast_ring(grid_dim, msg_size):
+        alpha = (math.log2(grid_dim) + grid_dim - 1)*perlmutter_params.inter_alpha
+        beta = ((2*(grid_dim-1))/grid_dim)*(msg_size/perlmutter_params.inter_beta)
+        return (alpha+beta)/1e6
+
+    grid_dim = int(math.sqrt(X.shape[0]))
+
+    bytes_mat = np.zeros(shape=(grid_dim, grid_dim))
+    times_mat = np.zeros(shape=(grid_dim, grid_dim))
+
+    # Populate bytes
     for _, x in X.iterrows():
-        ppn = x.PPN
-        nodes = x.Nodes
 
-        total_procs = ppn * nodes
+        rank = int(x["rank"])
 
         nnz_A, nnz_B = x["nnz-A"], x["nnz-B"] 
 
         bytes_A = nnz_A * 8 + nnz_A * 8 + x['m-A'] * 8
         bytes_B = nnz_B * 8 + nnz_B * 8 + x['m-B'] * 8
-
-        bcast_time_A = math.log2(total_procs)*perlmutter_params.inter_alpha + math.log2(total_procs)*(bytes_A / perlmutter_params.inter_beta)
-        bcast_time_B = math.log2(total_procs)*perlmutter_params.inter_alpha + math.log2(total_procs)*(bytes_B / perlmutter_params.inter_beta)
         
-        result_arr.append(bcast_time_A + bcast_time_B)
-    
-    return np.array(result_arr)
+        row_rank = rank % grid_dim
+        col_rank = rank // grid_dim
+
+        bytes_mat[row_rank, col_rank] = bytes_A + bytes_B
+
+    row_bytes = np.sum(bytes_mat, axis=1) # Storage for all submatrices in each prow
+    col_bytes = np.sum(bytes_mat, axis=0) # || but for each pcol
+
+    # Now, compute bcast times
+    for _, x in X.iterrows():
+
+        ppn = x["PPN"]
+        nodes = x["Nodes"]
+        rank = int(x["rank"])
+
+        # Reduce across rank's prow and pcol
+        row_rank = rank % grid_dim
+        col_rank = rank // grid_dim
+        
+        total_bytes = row_bytes[row_rank] + col_bytes[col_rank]
+
+        # Decide which bcast algorithm to use based on msg size
+        if total_bytes < 1e8 or True:
+            bcast_time = bcast_tree(grid_dim, total_bytes)
+        else:
+            bcast_time = bcast_ring(grid_dim, total_bytes)
+        
+        times_mat[row_rank, col_rank] = bcast_time
+
+    return times_mat.flatten()
 
 
 def spgemm_model(X):
-    bcast_pred = bcast_model(X)
-    mult_pred = loc_mult_model(X)
-    merge_pred = merge_model(X)
+    bcast_pred = np.array([]) 
+    for _, x in X.groupby(by=["Nodes","PPN"]):
+        bcast_pred = np.append(bcast_pred, (bcast_model(x)))
+    mult_pred = loc_mult_model(X, True)
+    merge_pred = merge_model(X, True)
     return bcast_pred + mult_pred + merge_pred
 
 
@@ -264,11 +357,32 @@ if __name__=="__main__":
     parser.add_argument("--epochs",  type=int)
     parser.add_argument("--nproblems", type=int)
     parser.add_argument("--neval", type=int)
+    parser.add_argument('--load', const=1, nargs='?', type=int)
 
     args = parser.parse_args()
     
     # Load in dataframe
-    df = load_gnn_df() 
+    if args.load:
+        df = load_gnn_df(features, labels) 
+
+        # Only problems with 1,2,4 nodes
+        all_problems = df['problem'].unique()
+        valid_problems = [p for p in all_problems if df[df['problem']==p]['Nodes'].unique().shape[0]==3]
+        df = df[df['problem'].isin(valid_problems)]
+        
+        # Make sure each grid is valid
+        
+        for p in valid_problems:
+            df_problem = df[df['problem']==p]
+            for _, d in df_problem.groupby(by=["Nodes", "PPN"]):
+                if not math.sqrt(d.shape[0]).is_integer():
+                    df.drop(labels=d.index)
+            
+
+        df.to_pickle("./master-df-gnn.pkl")
+    else:
+        df = pd.read_pickle("./master-df-gnn.pkl")
+    
     
     test_data, train_data = split(df, 0.1)
     
@@ -276,13 +390,13 @@ if __name__=="__main__":
         train_model_xgb(args, train_data, test_data)
     
     if args.eval=="mult":
-        eval_xgb(args, loc_mult_model, test_data, features)
+        eval_phase(args, test_data, loc_mult_model)
     elif args.eval=="merge":
-        eval_xgb(args, merge_model, test_data, features)
+        eval_phase(args, test_data, merge_model)
     elif args.eval=="bcast":
-        eval_xgb(args, bcast_model, test_data, features)
+        eval_phase(args,  test_data, bcast_model, False)
     elif args.eval=="spgemm":
-        eval_xgb(args, spgemm_model, df, features)
+        eval_phase(args,test_data, spgemm_model)
     
 
     
