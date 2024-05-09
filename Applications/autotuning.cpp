603aa23218442b5f0688bd1a8077f4c050745ecb
Hooninator <jbellavita@berkeley.edu>
2024-05-08 21:32:29 -0400

    autotuning driver changes for scaling tests

--- Applications/autotuning.cpp
+++ Applications/autotuning.cpp
@@ -28,8 +28,8 @@ int main(int argc, char ** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &n);
 
     {
-        autotuning::Init(autotuning::M_SLURM);
-        autotuning::Autotuner tuner(autotuning::perlmutterParams);
+        autotuning::Init(autotuning::M_OMPI);
+        autotuning::Autotuner tuner(autotuning::fractusParams);
 
         std::string matpathA(argv[1]);
         std::string matpathB(argv[2]);
@@ -66,16 +66,69 @@ int main(int argc, char ** argv) {
     
         int maxNodes = std::atoi(argv[4]);
         
+        // Test tuning
         stime = MPI_Wtime();
     
         autotuning::SpGEMMParams resultParams;
+
         resultParams = tuner.TuneSpGEMM2DAnalytical(A,B,matpathA,matpathB,maxNodes);
     
         etime = MPI_Wtime();
         tuningTime += (etime - stime);
+
+        autotuning::SpGEMMParams defaultParams = autotuning::SpGEMMParams::GetDefaultParams();
+        autotuning::SpGEMMParams scalingDownParams(1, std::floor(std::sqrt(defaultParams.GetPPN()))-1, 1);
+
+        /**** TEST SCALING DOWN ****/
+        /////////////////////////////
+
+        if (rank==0)
+            std::cout<<"Testing scaling down..."<<std::endl;
+
+        SpParMat<IT,UT,DER> Adown(grid );
+        Adown.ParallelReadMM(matpathA, true, maximum<double>());
+        Adown.ParallelWriteMM("./stomach-down-correct.mtx", true);
+
+        // Re-distribute from default to smaller process grid
+        DER * AdownLocalRedist = scalingDownParams.ReDistributeSpMat(Adown.seqptr(), defaultParams);
+
+        auto smallGrid = scalingDownParams.MakeGridFromParams();
+        if (smallGrid!=NULL) 
+        {
+            SpParMat<IT,UT,DER> AdownRedist(AdownLocalRedist, smallGrid);
+            AdownRedist.ParallelWriteMM("./stomach-down.mtx", true );
+        }
+
+        if (rank==0)
+            std::cout<<"Scaling down done"<<std::endl;
+
+        ///////////////////////////
+        /**** END TEST SCALING DOWN ****/
+       
+
+        /**** TEST SCALING UP ****/
+        ///////////////////////////
+        if (rank==0)
+            std::cout<<"Testing scaling up..."<<std::endl;
+
+        std::cout<<AdownLocalRedist->getnnz()<<std::endl;
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        // Re-redistribute from smaller to default process grid
+        DER * AupLocalRedist = defaultParams.ReDistributeSpMat(AdownLocalRedist, scalingDownParams);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        if (rank==0)
+            std::cout<<"Scaling up done"<<std::endl;
+
+        /**** END TEST SCALING UP ****/
+        ///////////////////////////
     
         bool doMult = (bool)(std::atoi(argv[5]));
     
+        //Test multiplication with redistributed matrices
         if (doMult) {
 
             stime = MPI_Wtime();
@@ -85,14 +138,20 @@ int main(int argc, char ** argv) {
             SpGEMMTime += (etime - stime);
     
             auto tunedGrid = resultParams.MakeGridFromParams();
+            DER * ARedist = resultParams.ReDistributeSpMat(A.seqptr(), defaultParams);
+            DER * BRedist = resultParams.ReDistributeSpMat(B.seqptr(),  defaultParams);
     
             if (tunedGrid!=NULL) {
                 {
                     stime = MPI_Wtime();
+                    /*
                     SpParMat<IT,UT,DER> ATuned(tunedGrid);
                     SpParMat<IT,UT,DER> BTuned(tunedGrid);
                     ATuned.ParallelReadMM(matpathA,true, maximum<double>());
                     BTuned.ParallelReadMM(matpathB,true, maximum<double>());
+                    */
+                    SpParMat<IT, UT, DER> ATuned(ARedist, tunedGrid);
+                    SpParMat<IT, UT, DER> BTuned(BRedist, tunedGrid);
                     etime = MPI_Wtime();
         
                     redistTime = (etime - stime);
@@ -102,9 +161,10 @@ int main(int argc, char ** argv) {
                     etime = MPI_Wtime();
         
                     tunedSpGEMMTime += (etime - stime);
+
                 }
 
-                if (rank==0) {
+                if (tunedGrid->GetRank()==0) {
                     std::cout<<"SpGEMM Time: "<<SpGEMMTime<<std::endl;
                     std::cout<<"Tuned SpGEMM Time: "<<tunedSpGEMMTime<<std::endl;
                     std::cout<<"Tuning Time: "<<tuningTime<<std::endl;
