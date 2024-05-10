305a7c013bd03ea62b9dcea09cd96120f8150af3
Hooninator <jbellavita@berkeley.edu>
2024-05-10 16:53:36 -0400

    add test script + minor changes to tuningredist

--- ReleaseTests/TuningRedist.cpp
+++ ReleaseTests/TuningRedist.cpp
@@ -14,6 +14,12 @@
 #define THREADED
 
 using namespace combblas;
+using namespace combblas::autotuning;
+
+typedef int64_t IT;
+typedef double UT;
+typedef SpDCCols<IT,UT> DER;
+typedef PlusTimesSRing<UT,UT> PTTF;
 
 unsigned int NextSmallestPerfectSquare(unsigned int n) {
     // Find the square root of the input number
@@ -28,78 +34,111 @@ unsigned int NextSmallestPerfectSquare(unsigned int n) {
     return root * root;
 }
 
-template <typename IT, typename UT,typename  DER>
-void RunRedistTest(SpParMat<IT,UT,DER>& A, std::string matpathA, int rank)
+
+void RunRedistTest(std::string matpathA, int rank, 
+                    SpGEMMParams& startParams, 
+                    SpGEMMParams& downParams)
 {
 
-    autotuning::SpGEMMParams defaultParams = autotuning::SpGEMMParams::GetDefaultParams();
-    autotuning::SpGEMMParams scalingDownParams(1, NextSmallestPerfectSquare(defaultParams.GetPPN()-1), 1);
+    auto startGrid = startParams.MakeGridFromParams();
+    auto downGrid = downParams.MakeGridFromParams();
 
-    auto grid = A.getcommgrid();
+    if (startGrid!=NULL) {
+        SpParMat<IT,UT,DER> A(startGrid);
+        A.ParallelReadMM(matpathA, true, maximum<double>());
 
-    /**** TEST SCALING DOWN ****/
-    /////////////////////////////
+        /**** TEST SCALING DOWN ****/
+        /////////////////////////////
 
-    if (rank==0)
-        std::cout<<"Testing scaling down..."<<std::endl;
+        if (rank==0)
+            std::cout<<"Testing scaling down..."<<std::endl;
 
-    A.ParallelWriteMM("./stomach-correct.mtx", true);
+        A.ParallelWriteMM(matpathA+"-correct.mtx", true);
 
-    // Re-distribute from default to smaller process grid
-    DER * AdownLocalRedist = scalingDownParams.ReDistributeSpMat(A.seqptr(), defaultParams);
+        // Re-distribute from default to smaller process grid
+        DER * AdownLocalRedist = downParams.ReDistributeSpMat(A.seqptr(), startParams);
 
-    auto smallGrid = scalingDownParams.MakeGridFromParams();
-    if (smallGrid!=NULL) 
-    {
-        DER * AdownLocalRedistCpy = new DER(*AdownLocalRedist);
-        SpParMat<IT,UT,DER> AdownRedist(AdownLocalRedistCpy, smallGrid);
-        AdownRedist.ParallelWriteMM("./stomach-down.mtx", true );
+        if (downGrid!=NULL) 
+        {
+            DER * AdownLocalRedistCpy = new DER(*AdownLocalRedist);
+            SpParMat<IT,UT,DER> AdownRedist(AdownLocalRedistCpy, downGrid);
+            AdownRedist.ParallelWriteMM(matpathA+"-down.mtx", true );
+        }
+
+        MPI_Barrier(A.getcommgrid()->GetWorld());
+
+        if (rank==0)
+            std::cout<<"Scaling down done"<<std::endl;
+
+        ///////////////////////////
+        /**** END TEST SCALING DOWN ****/
+       
+        MPI_Barrier(A.getcommgrid()->GetWorld());
+
+        /**** TEST SCALING UP ****/
+        ///////////////////////////
+        if (rank==0)
+            std::cout<<"Testing scaling up..."<<std::endl;
+
+        // Re-redistribute from smaller to default process grid
+        DER * AupLocalRedist = startParams.ReDistributeSpMat(AdownLocalRedist, downParams);
+
+
+        if (startGrid!=NULL)
+        {
+            DER * AupLocalRedistCpy = new DER(*AupLocalRedist);
+            SpParMat<IT,UT,DER> AupRedist(AupLocalRedistCpy, startGrid);
+            AupRedist.ParallelWriteMM(matpathA+"-up.mtx", true);
+        }
+
+        MPI_Barrier(A.getcommgrid()->GetWorld());
+
+        if (rank==0)
+            std::cout<<"Scaling up done"<<std::endl;
+
+        /**** END TEST SCALING UP ****/
+        ///////////////////////////
     }
-
+        
     MPI_Barrier(MPI_COMM_WORLD);
-
-    if (rank==0)
-        std::cout<<"Scaling down done"<<std::endl;
-
-    ///////////////////////////
-    /**** END TEST SCALING DOWN ****/
-   
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    /**** TEST SCALING UP ****/
-    ///////////////////////////
-    if (rank==0)
-        std::cout<<"Testing scaling up..."<<std::endl;
-
-    // Re-redistribute from smaller to default process grid
-    DER * AupLocalRedist = defaultParams.ReDistributeSpMat(AdownLocalRedist, scalingDownParams);
+}
 
 
-    if (grid!=NULL)
-    {
-        DER * AupLocalRedistCpy = new DER(*AupLocalRedist);
-        SpParMat<IT,UT,DER> AupRedist(AupLocalRedistCpy, grid);
-        AupRedist.ParallelWriteMM("./stomach-up.mtx", true);
+void parse_args(int argc, char ** argv,
+                std::string& matpath,
+                int& startNodes, int& startPPN,
+                int& downNodes, int& downPPN)
+{
+    for (int i=1; i<argc-1; i++) {
+
+        if (!strcmp(argv[i], "--startnodes"))
+            startNodes = std::atoi(argv[i+1]);
+        if (!strcmp(argv[i], "--downnodes"))
+            downNodes = std::atoi(argv[i+1]);
+        if (!strcmp(argv[i], "--startppn"))
+            downNodes = std::atoi(argv[i+1]);
+        if (!strcmp(argv[i], "--downppn"))
+            downNodes = std::atoi(argv[i+1]);
+        if (!strcmp(argv[i], "--matpath"))
+            downNodes = std::atoi(argv[i+1]);
+
     }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    if (rank==0)
-        std::cout<<"Scaling up done"<<std::endl;
-
-    /**** END TEST SCALING UP ****/
-    ///////////////////////////
-    
-    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 
 int main(int argc, char ** argv) 
 {
     
-    /* ./<binary> <path/to/mat> */
+    /* ./<binary> --matpath <path/to/mat> --startnodes <i> --startppn <j>
+     * --downnodes <k> --downppn <l> */
     
-    assert(argc>1);
+    assert(argc>10);
+
+    std::string matpathA;
+    int startNodes; int downNodes;
+    int startPPN; int downPPN;
+
+    parse_args(argc, argv, matpathA, startNodes, startPPN, downNodes, downPPN);
     
     int rank; int n;
     MPI_Init(&argc, &argv);
@@ -110,20 +149,11 @@ int main(int argc, char ** argv)
         autotuning::Init(autotuning::M_OMPI);
         autotuning::Autotuner tuner(autotuning::fractusParams);
 
-        std::string matpathA(argv[1]);
 
-        std::shared_ptr<CommGrid> grid;
-        grid.reset(new CommGrid(MPI_COMM_WORLD,0,0));
-        
-        typedef int64_t IT;
-        typedef double UT;
-        typedef SpDCCols<IT,UT> DER;
-        typedef PlusTimesSRing<UT,UT> PTTF;
+        SpGEMMParams startParams(startNodes, startPPN, 1);
+        SpGEMMParams downParams(downNodes, downPPN, 1);
 
-        SpParMat<IT,UT,DER> A(grid);
-        A.ParallelReadMM(matpathA, true, maximum<double>());
-
-        RunRedistTest(A, matpathA, rank);
+        RunRedistTest(matpathA, rank, startParams, downParams);
 
     }
 
---
+++ ReleaseTests/run-redist-test.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+
+mtx=$1
+startnodes=$2
+downnodes=$3
+startppn=$4
+downppn=$4
+
+>debug-down.out
+>$mtx-down.mtx
+
+>debug-up.out
+>$mtx-up.mtx
+
+>$mtx-correct.mtx
+mpirun -n $startppn ReleaseTests/TuningRedist --mat ../redist_test_matrices/$mtx/$mtx.mtx --startnodes $startnodes --downnodes $downnodes --startppn $startppn --downppn $downppn
+
+
+DIFFUP=$(diff ${mtx}-correct.mtx ${mtx}-up.mtx)
+if [ "$DIFFUP" == "" ]; then 
+    echo "Scaling up test passed!"
+    rm  $mtx-up.mtx
+else
+    echo "Scaling up test failed"
+fi
+
+DIFFDOWN=$(diff ${mtx}-correct.mtx ${mtx}-down.mtx)
+if [ "$DIFFDOWN" == "" ]; then
+    echo "Scaling down test passed!"
+    rm $mtx-down.mtx
+else
+    echo "Scaling down test failed"
+fi
+
+
